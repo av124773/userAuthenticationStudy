3119578583db00dbc2a56f3b4efa8ca839d950c1
av124773 <av124773@gmail.com>
2023-03-23 14:53:53 +0800

    feat: add check user function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const User = require('./models/user')
 
 if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -11,7 +13,7 @@ const app = express()
 const PORT = 3001
 
 // mongoose setting
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 const db = mongoose.connection
 db.on('error', () => {
@@ -23,10 +25,32 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true}))
 
-// setting route
+// route config
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/login', (req, res) => {
+  const {email, password} = req.body
+  return User.findOne({email: email})
+    .lean()
+    .then(user => {
+      if (user !== null) {
+        console.log(user.firstName)
+        res.render('welcome', {user})
+        // res.redirect('get/welcome')
+      } else {
+        res.redirect('/')
+      }
+    })
+    .catch(error => console.log(error))
+})
+
+app.get('/welcome', (req, res) => {
+  const { email, password } = req.body
+  console.log(email)
 })
 
 app.listen(PORT, () => {
